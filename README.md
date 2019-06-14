4acf35176d8123e7cb7b26a7766a980f8458c1bc
Dave Martin <justanotherdavemartin@gmail.com>
2019-06-15 00:23:36 +0100

    updated app

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # wp-micro-frontend
 
-Embed [Create React App](https://facebook.github.io/create-react-app/) with a Wordpress shortcode
+Embed [Create React App](https://facebook.github.io/create-react-app/) with a Wordpress shortcode `[wpmicrofe][/wpmicrofe]`
 
-Create your app in `/js` - this plug in fetches all scripts in `/js/build/static/js`.
+Create your app in `/js` - this plug in fetches all scripts in `/js/build/static/js`. When the app is built check the correct js is being enqueued.  (Further iterations might pick up all scripts in the build folder).
--- wp-micro-frontend.php
+++ wp-micro-frontend.php
@@ -6,8 +6,8 @@
   function wpmicrofe_shortcodes_init() {  
     add_shortcode('wpmicrofe', function() {
       wp_enqueue_script('reactAppRuntime', plugins_url('/js/build/static/js/runtime~main.c87cc1eb.js', __FILE__)); 
-      wp_enqueue_script('reactApp', plugins_url('/js/build/static/js/2.34b4fde6.chunk.js', __FILE__));
-      wp_enqueue_script('reactAppMain', plugins_url('/js/build/static/js/main.f96f5104.chunk.js', __FILE__)); 
+      wp_enqueue_script('reactApp', plugins_url('/js/build/static/js/2.c272f57a.chunk.js', __FILE__));
+      wp_enqueue_script('reactAppMain', plugins_url('/js/build/static/js/main.0e49e05e.chunk.js', __FILE__)); 
       return sprintf("<div id=\"root\"></div>");
     });
   }
