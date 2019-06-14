bfb0f668851807bc89701a6c878642ed848c65b6
Dave Martin <justanotherdavemartin@gmail.com>
2019-06-14 11:27:55 +0100

    got react up and running

--- wp-micro-frontend.php
+++ wp-micro-frontend.php
@@ -3,11 +3,12 @@
  * Plugin Name: wp-micro-frontend
  */
 
-  function wpmicrofe_shortcodes_init() {
-      
+  function wpmicrofe_shortcodes_init() {  
     add_shortcode('wpmicrofe', function() {
-      wp_enqueue_script('reactApp', plugins_url('/js/app.js', __FILE__));  
-      return sprintf("<div id=\"reactApp\"></div>");
+      wp_enqueue_script('reactAppRuntime', plugins_url('/js/build/static/js/runtime~main.c87cc1eb.js', __FILE__)); 
+      wp_enqueue_script('reactApp', plugins_url('/js/build/static/js/2.34b4fde6.chunk.js', __FILE__));
+      wp_enqueue_script('reactAppMain', plugins_url('/js/build/static/js/main.f96f5104.chunk.js', __FILE__)); 
+      return sprintf("<div id=\"root\"></div>");
     });
   }
 
